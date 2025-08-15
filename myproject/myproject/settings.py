c0942d5ced572b1b4f641187279e95c65ea79550
ZaramNelo <zaramboy2017@gmail.com>
2025-08-15 14:35:08 -0400

    alright

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zaramboy2017@gmail.com'
-EMAIL_HOST_PASSWORD = 'vnap vcef oumg rcdo'  
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
